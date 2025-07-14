ff2853302b2cada289d8463ad52755b4295c8835
Mirka McIntyre <mirka.kiana@gmail.com>
2025-07-14 15:09:06 -0400

    Created a class for the form inputs.

---
+++ js/class.js
@@ -0,0 +1,10 @@
+// making a class
+
+class Order {
+    //Added constructor to pass in parameters
+    constructor(name, size, flavour) {
+        this.name = name;
+        this.size = size;
+        this.flavour = flavour;
+    }
+}
