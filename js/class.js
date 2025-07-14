be4cb27d95393edfac18c3d39a25c1a3566788d9
Mirka McIntyre <mirka.kiana@gmail.com>
2025-07-14 16:43:16 -0400

    Added method to return the sentence.

--- js/class.js
+++ js/class.js
@@ -6,5 +6,12 @@ class Order {
         this.name = name;
         this.size = size;
         this.flavour = flavour;
-    }
+        
+    };
+
+    //Add method to return the sentence
+        output() {
+            return `${this.name} ordered a ${this.size} ${this.flavour} smoothie.`;
+        } 
 }
+
